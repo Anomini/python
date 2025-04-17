7aebdb1e5ee55c46b4c160291881e832144e39cd
Anomini <anomini12342000@gmail.com>
2025-04-18 01:58:55 +0300

    Update setup.py

--- y/setup.py
+++ y/setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup, find_packages
+lfrom setuptools import setup, find_packages
 
 setup(
-    name='y',
-    version='0.1',
-    packages=find_packages(),
-    install_requires=[],
+    name='y',  # שם החבילה שלך
+    version='0.1',  # גרסה של החבילה
+    packages=find_packages(),  # זה ימצא את כל החבילות בתיקיות בתוך המאגר
+    install_requires=[],  # אם יש תלות, ציין פה חבילות נוספות
 )
