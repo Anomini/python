a2d492e4a468d75e85f7a1df7aeddaac0dca3784
Anomini <anomini12342000@gmail.com>
2025-04-20 01:28:40 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='kolayair',
-    version='0.1.0',
-    py_modules=['l'],  # קובץ בשם my_module.py
+    version='1.1.1',
+    packages=find_packages()
 )
