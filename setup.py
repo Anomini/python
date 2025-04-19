efbf9d32df0b343bcc6a1bb5e44567f125d85807
Anomini <anomini12342000@gmail.com>
2025-04-20 01:45:04 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name='kolayair',
     version='1.1.2',
-    packages=find_packages()
+    packages=find_packages(),  # תמצא את כל החבילות בתיקיות כמו Y
 )
